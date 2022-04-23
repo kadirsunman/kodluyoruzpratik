953d656c3bca53db43a2640b171b0fbb591ce552
Kadir Sunman <kadirsunman@gmail.com>
2022-04-23 17:14:41 +0300

    Pratik RecursivePow

---
+++ src/Loops/RecursivePow.java
@@ -0,0 +1,24 @@
+package Loops;
+
+import java.util.Scanner;
+
+public class RecursivePow {
+    public static int gettingBase(int value, int pow)
+    {
+        if(pow == 0)
+        {
+            return 1;
+        }
+        pow--;
+        return value *= gettingBase(value, pow);
+    }
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Taban değeri giriniz : ");
+        int base = scan.nextInt();
+        System.out.print("Taban değeri giriniz : ");
+        int pow = scan.nextInt();
+        System.out.println(gettingBase(base, pow));
+    }
+}
